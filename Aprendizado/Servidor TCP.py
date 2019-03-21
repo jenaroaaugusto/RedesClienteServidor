b3c41d810155cec5ea0d28bbb4845e9ac20b19fe
Jenáro Augusto <jenaroaaugusto@outlook.com>
2019-03-20 21:41:57 -0300

    Aprendizagem

---
+++ Aprendizado/Servidor TCP.py
@@ -0,0 +1,26 @@
+import socket
+import datetime
+HOST = '127.0.0.1'              # Endereco IP do Servidor
+PORT = 5000            # Porta que o Servidor esta
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+orig = (HOST, PORT)
+tcp.bind(orig)
+tcp.listen(1)
+while True:
+    con, cliente = tcp.accept()
+    print 'Concetado por', cliente
+    while True:
+        msg= con.recv(1024)
+        print msg
+        # if not msg: break
+        # resposta=input()
+        if msg == 'd':
+            # x= datetime.datetime.now()
+            # print (x)
+            # h=str(x)
+            h="jenaro" 
+            con.sendall(h)
+        elif msg  =='a':
+            print cliente, msg
+    print 'Finalizando conexao do cliente', cliente
+con.close()
--- requisicao.py
+++ requisicao.py
@@ -1,7 +1,8 @@
-#coding: utf-8
+#encoding: utf-8
 
 import socket
 import re 
+import ssl
 
 def requisicao():
     arq=[]
@@ -9,46 +10,66 @@ def requisicao():
     # req = b"GET / HTTP/1.1\nHost: stackoverflow.com\n\n"
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # s.connect(("stackoverflow.com", 80))   
-    s.connect(("example.com", 80))
+    s.connect(("www.youtube.com", 80))
     # User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36
-    s.sendall(bytes("GET / HTTP/1.1\r\nHost:example.com\r\n\r\n",'utf8'))
-        # ,'utf8'
-    # print("passou\n",s.proto)
-    # input()
-    # 65536
+    s.sendall(b"GET / HTTP/1.1\r\nHost:www.youtube.com\r\n\r\n")
+    # 65536 francoerangelimoveis
     resposta=s.recv(65536)
-    # while True:
-    # 'utf-8'
-    #     if resposta:
+    # resposta.decode('cp1252')
+    # .encode('utf-8')
     s.close()
-    dados=str(resposta)
-   
-    
-    print(dados,'\n')
 
-    
-    # .decode('utf8')
-    # arq2.append(dados)
-    # #     # else:
-    # #     #     break 
-    # print(arq2,"\n")
-    # input()
-    # print(type(resposta),"\n")
-     
+    dados=str(resposta,'utf-8')
+    # dados=dados.encode('utf-8')
+    # dados=dados.decode('cp1252').encode('utf-8') 
+    # dados=dados.encode('utf-8')
+    print(dados,'\n')
+  
     # Verifica o codigodo de resposta
     arq=''+chr(resposta[9])+chr(resposta[10])+chr(resposta[11])
     if  arq == '301':
-        print("Aqui")
-        input()
+        print('ok')
+        # for i in range(0, len(dados)):
+        #     if dados[i]=='L' and dados[i+1]=='o' and dados[i+2] =='c' and dados[i+1] == 'a ' and dados[i+1]=='t'and dados[i+1] == 'i' and dados[i+1] == 'o' and dados[i+1] =='n'
+        x=dados.index('https://')
+        # print(dados)
+        # print(x)
+        # for i in range(x,len(dados)):
+        #     if dados[i] =="\n":
+        #         print(dados[i],"Valor I",i)
+            
+        #         print ('aqui')
+        HOST = "www.youtube.com"
+        PORT = 443
+
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s_sock = context.wrap_socket(s, server_hostname=HOST)
+        s_sock.connect((HOST, 443))
+        s_sock.send(" GET / HTTP/1.1\r\nHost: www.youtube.com\r\n\r\n ".encode())
+
+        while True:
+            data = s_sock.recv(2048)
+            if ( len(data) < 1 ) :
+                break
+            print(data)
+
+        s_sock.close()    
+        
     elif arq != '200' :
         print("erro")
         exit()
     print(arq,"\n")
+    input()
     
+
+
+
+
     separador=0
     # z= len(dados)
     for x in range(0,len(dados)):
-        if dados[x]=='<' and dados[x+1]=='!':  
+        if dados[x]=='<' and dados[x+1]=='!' and (dados[x+2]=='d' or dados[x+2]=='D') :  
             print('ok')
             separador=x
 
@@ -62,11 +83,17 @@ def requisicao():
     # input() 
     # dados.replace("\n"," ")
     corpo=re.sub(r'[\\]+[n]'," ",corpo)
+    corpo=re.sub(r'[\\]+[t]'," ",corpo)
+    corpo=re.sub(r"[\\]+[']"," ",corpo)
+    corpo=re.sub(r'[\\]+[r]'," ",corpo)
+    # corpo=corpo.decode('utf-8')
+
     print(corpo)
 
     f = open("index.html", 'w')
-   
-    f.write(corpo)
+    # f.write(str(corpo,'latin1'))
+    # f.write(str(corpo))
+    f.write(str(corpo))
     f.close()
     
 requisicao()   
