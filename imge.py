8c1e43a5185f2695ef17d044874717fda78529bc
Jenáro Augusto <jenaroaaugusto@outlook.com>
2019-04-08 16:10:19 -0300

    Funciona Pegar infomração salva em arquivo falta salvar em diretorios.

--- imge.py
+++ imge.py
@@ -3,39 +3,48 @@ import select
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # http:///
-host="www.opopularjm.com.br"
-path='wp-content/uploads/2019/03/54730591_2023596907688141_5192235201639481344_n.jpg'
+host="www.gazetadesaojoaodelrei.com.br"
+path='/site'
+path=path+'/'
 host, path = host.encode(), path.encode()
+# print(path)
+# input()
 s.connect((host, 80))
-s.sendall(b'GET /%b HTTP/1.1\r\nHOST: %b\r\n\r\n' % (path,host))
+s.sendall(b'GET %b HTTP/1.1\r\nHOST: %b\r\n\r\n' % (path,host))
 
 reply = b''
 
 while select.select([s], [], [], 3)[0]:
-    data = s.recv(2048)
+    data = s.recv(65536)
     if not data: break
     reply += data
 
 headers =  reply.split(b'\r\n\r\n')[0]
 image = reply[len(headers)+4:]
-print(headers)
-input()
-path=path.decode()
-extensoes = ['.txt', '.mp3', '.pdf', '.html', '.jpg', '.png']
-if len(path)!=0 :
-    path=path.split('/')
-    print("ok")
-    for arquivo in path:
-        if arquivo[-4:] in extensoes:
-            A='Downloads/'
-    B="Downloads/arquivo_generico"
+dados=str(image, ' utf-8 ' )
+print(dados)
+# print(image)
+print('\n')
+# print('\n apos o texto')
+
+
+
+# path=path.decode()
+# extensoes = ['.txt', '.mp3', '.pdf', '.html', '.jpg', '.png']
+# if len(path)!=0 :
+#     path=path.split('/')
+#     print("ok")
+#     for arquivo in path:
+#         if arquivo[-4:] in extensoes:
+#              'Downloads/'
+#     return "Downloads/arquivo_generico"
     
 
 
 
-input()
+
 
 # save image
-f = open('image6.jpg', 'wb')
+f = open('image7.html', 'wb')
 f.write(image)
 f.close()
--- requisicao.py
+++ requisicao.py
@@ -2,69 +2,142 @@
 
 import socket
 import re 
-import ssl
+import select
+def caminho_comDados(path,headers):
+    path=path.decode()
+    extensoes = ['.txt', '.mp3', '.pdf', '.html', '.jpg', '.png']
+    path=path.split('/')
+    print("ok")
+    for arquivo in path:
+        if arquivo[-4:] in extensoes:
+            return 'Downloads-'
+    return "Downloads-arquivo_generico"
+def CaminhosSemArquivo(mensagem,host):    
+    print('Host',host)
+    host=host.decode()
+    posi=host.index("www")
+    
+    nome=host[posi:len(host)] 
+    
 
+    return "Downloads-"+nome+".html"
 def requisicaohost(host,path,porta):
     arq=[]
     arq2=[]
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
     s.connect((host,int(porta)))
-    
+    auxipath=path
     path=path+'/'
     host, path = host.encode(), path.encode()
  
     s.sendall(b'GET %b HTTP/1.1\r\nHost:%b\r\n\r\n'%(path,host))
-    resposta=s.recv(65536)
-    s.close()
+    resposta=s.recv(512000)
+    
 
     dados=str(resposta,'utf-8')
-    
-    print(dados,'\n')
-    # dados
-  
-    # Verifica o codigodo de resposta
-    arq=''+chr(resposta[9])+chr(resposta[10])+chr(resposta[11])
-    if  arq == '301':
-        print('ERRO 301')    
-    elif arq != '200' :
-        print("erro")
-        exit()
-    print(arq,"\n")
-    input()
-    
+    print(len(dados))
+    reply = b''
 
+    while select.select([s], [], [], 3)[0]:
+        data = s.recv(65536)
+        if not data: break
+        reply += data
 
+    headers =  reply.split(b'\r\n\r\n')[0]
+    corpoitens = reply[len(headers)+4:]
+    # print(corpoitens)
+    # input()
 
-
+    s.close()
+    # print(headers)
+    # input()
+    # print(len(auxipath))
+    # print(auxipath)
+    # input()
     separador=0
-    # z= len(dados)
-    for x in range(0,len(dados)):
-        if dados[x]=='<' and dados[x+1]=='!' and (dados[x+2]=='d' or dados[x+2]=='D') :  
-            print('ok')
-            separador=x
 
-            print(x)
+    # for x in range(0,len(dados)):
+    #     if dados[x]=='<' and dados[x+1]=='!' and (dados[x+2]=='d' or dados[x+2]=='D') :  
+    #         print('ok')
+    #         separador=x
+
+    #         print(x)
+    if (dados.find("!DOCTYPE")!=-1):
+        separador=dados.index("!DOCTYPE")
+    elif(dados.find("!doctype")!=-1):
+         separador=dados.index("!doctype")
+    # separador=dados.index("!DOCTYPE") 
     print(separador,'\n')
     cabecalho=dados[0:separador]
-    corpo=dados[separador:len(dados)]
-    # print(cabecalho,"\n")    
+    fim=dados.index("</html>")
+    corpo=dados[separador:fim]
     
-    # print(corpo,"\n")
-    # input() 
-    # dados.replace("\n"," ")
     corpo=re.sub(r'[\\]+[n]'," ",corpo)
     corpo=re.sub(r'[\\]+[t]'," ",corpo)
     corpo=re.sub(r"[\\]+[']"," ",corpo)
     corpo=re.sub(r'[\\]+[r]'," ",corpo)
-    # corpo=corpo.decode('utf-8')
-
-    print(corpo)
-
-    f = open("index2.html", 'w')
-    # f.write(str(corpo,'latin1'))
-    # f.write(str(corpo))
-    f.write(str(corpo))
-    f.close()
     
-# requisicao()   
+    print(type(corpo))
+
+    if len(auxipath)!=0:
+        arqnome = caminho_comDados(path,headers)
+        saida = open(arqnome,'wb')
+        saida.write(corpoitens)
+        saida.close()
+    else:
+        arqnome= CaminhosSemArquivo(corpo,host)
+        saida = open(arqnome,'w')
+        print('ok') 
+        # informacao=str(corpo, ' utf-8 ' ) 
+        saida.write(str(corpo))
+        saida.close()
+
+
+
+#     dados = str (resposta, ' utf-8 ' )
+
+#     print(dados,'\n')
+#     # dados
+  
+#     # Verifica o codigodo de resposta
+#     arq=''+chr(resposta[9])+chr(resposta[10])+chr(resposta[11])
+#     if  arq == '301':
+#         print('ERRO 301')    
+#     elif arq != '200' :
+#         print("erro")
+#         exit()
+#     print(arq,"\n")
+#     input()
+    
+
+
+
+
+#     separador=0
+
+#     for x in range(0,len(dados)):
+#         if dados[x]=='<' and dados[x+1]=='!' and (dados[x+2]=='d' or dados[x+2]=='D') :  
+#             print('ok')
+#             separador=x
+
+#             print(x)
+#     print(separador,'\n')
+#     cabecalho=dados[0:separador]
+#     corpo=dados[separador:len(dados)]
+    
+#     corpo=re.sub(r'[\\]+[n]'," ",corpo)
+#     corpo=re.sub(r'[\\]+[t]'," ",corpo)
+#     corpo=re.sub(r"[\\]+[']"," ",corpo)
+#     corpo=re.sub(r'[\\]+[r]'," ",corpo)
+    
+
+#     print(corpo)
+
+#     f = open("index2.html", 'w')
+#     # f.write(str(corpo,'latin1'))
+#     # f.write(str(corpo))
+#     f.write(str(corpo))
+#     f.close()
+    
+# # requisicao()   
