dd110c129aab67bc38f80a85068cdea51ed99f1a
Jenaro Augusto <jenaroaaugusto@outlook.com>
2019-05-24 00:29:51 -0300

    Agora envia imagens e dados para navegador

--- Threading Servidor.py
+++ Threading Servidor.py
@@ -6,10 +6,13 @@ def atividadeconecao(cliente,con):
     print("Cliente conectado: IP ",cliente)
     while True:
         msg = con.recv(1024)
+        estadonatural=copy.copy(msg)
         msg=msg.decode()
         print("No mensagem",msg)
+        print("i",msg.index("T"),'x',msg.index("HTTP"))
+        print("Estado natural", estadonatural)
         auxmsg=msg.split()
-        print("No aux2",auxmsg)
+        
         busca=copy.deepcopy(auxmsg[1])
         if busca == '/' :
             arq = open("estouaqui.html",'r')
@@ -20,9 +23,28 @@ def atividadeconecao(cliente,con):
             informacao=informacao.encode('utf-8')
             cabeca=cabeca+informacao
             con.send(cabeca)
+            
             break
         # print("Copia",busca)
         # # exit()
+        else:
+            print("No mensagem")
+            print("i",msg.index("T"),'x',msg.index("HTTP"))
+            i=msg.index("/") 
+            x=msg.index("HTTP")
+            requerido=msg[i:x]
+            print(requerido)
+            cabeca=''
+            cabeca="HTTP/1.1/ 200 OK\r\nHost:127.0.0.1:5000\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41\r\nAccept:  image/webp,image/apng,image/*,*/*\r\nAccept-Encoding: deflate\r\nAccept-Language: en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7\r\n\r\n"
+            cabeca=cabeca.encode('utf-8')
+            arq = open("servidor"+requerido,'rb')
+            informacao=arq.read()
+            cabeca=cabeca+informacao
+            con.send(cabeca)
+
+            print("OK")
+
+            
         if "Hello" in msg:
             arq = open("servidor/index.html",'r')
             informacao= arq.read()
@@ -43,7 +65,8 @@ def atividadeconecao(cliente,con):
         
             arq.close()
             break
-        arquivos=['/index.html','/logo.png','/casa.txt']
+        arquivos=['/index.html','/logo.png','/casa.txt','/icons/blank.gif']
+        
         if busca in arquivos:
             
             arq = open("servidor"+busca,'r')
@@ -54,10 +77,12 @@ def atividadeconecao(cliente,con):
             arq.close()
             break
 
-        if msg =="exit":
+        if msg =="exit": 
             break 
             exit()
-        print (cliente, msg)
+        
+        print ("Sou eu",cliente, msg)
+        break
     print ('Finalizando conexao do cliente', cliente )
     exit()
     con.close()
