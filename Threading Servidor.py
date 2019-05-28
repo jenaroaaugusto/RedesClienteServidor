9ada21a5846f8b0b3f715c5e0cf8ec7ff144ada8
Jenaro Augusto <jenaroaaugusto@outlook.com>
2019-05-28 17:52:36 -0300

    Funciona rastreamento na pasta local

--- Threading Servidor.py
+++ Threading Servidor.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import threading
 import copy
+import os 
 def tipo(requerido):
     if requerido.endswith(".jpg"):
         mimetype = 'image/jpg'
@@ -22,24 +23,49 @@ def tipo(requerido):
     return mimetype
 def atividadeconecao(cliente,con):
     print("Cliente conectado: IP ",cliente)
+    
+    # print(diretorio)
+    # exit()
+    href=[]
+    
     while True:
+        diretorio=os.listdir("servidor")
         msg = con.recv(1024)
         estadonatural=copy.copy(msg)
         msg=msg.decode()
         print("No mensagem",msg)
+        if msg =="":
+            break
         print("i",msg.index("T"),'x',msg.index("HTTP"))
         print("Estado natural", estadonatural)
         auxmsg=msg.split()
         
         busca=copy.deepcopy(auxmsg[1])
         if busca == '/' or busca=='/index':
-            arq = open("estouaqui.html",'r')
+            print("Aqui")
+            # arq = open("estouaqui.html",'r')
+            # print(diretorio)
+            # print(type(diretorio))
+        
+            # exit()
+            # for j in range(len(diretorio)):
+            for i in diretorio:
+                print(i)
+                ok="<li><a href=/"+i+">"+i+"/</a></li>"
+                href.append(ok)
+            lista=' '.join(href)
+            lista="<ul>"+lista+"</ul>"
+                 
+            info="<html <head> <title>Index of /dist</title></head><body><h1>NINA Directory</h1><h2>Arquivos</h2><dl><dt>Trabalho Pratico para Disciplina de Redes de Computadores 2019/1 UFSJ</dt><dt>Para rodar o Servidor e o Cliente respectivamente:</dt><dd>python Threading Servidor.py</dd><dd>python navegador.py</dd></dl><pre><hr><p>"+lista+"</p></pre><hr><p> Redes Cliente Servidor Python</p></body></html>"
             
-            informacao=arq.read()
             cabeca="HTTP/1.1/ 200 OK\r\nHost:127.0.0.1:5000\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41\r\nAccept: text/html\r\nAccept-Encoding: deflate\r\nAccept-Language: en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7\r\n\r\n"
             cabeca=cabeca.encode('utf-8')
-            informacao=informacao.encode('utf-8')
-            cabeca=cabeca+informacao
+            
+            info=info.encode('utf-8')
+             
+            cabeca=cabeca+info
+            # cabeca=cabeca+informacao
+            print("E a cabeça",cabeca)
             con.send(cabeca)
             
             break
@@ -103,6 +129,7 @@ def conectado(porta,caminho):
             args=(cliente,con)
         )
         conecoes.start()
+        # exit()
 
 if __name__ == "__main__":
     HOST='127.0.0.1'
--- navegador.py
+++ navegador.py
@@ -50,50 +50,6 @@ def busca(url):
       if "www."not in host: host="www."+host
       ende.requisicaohost(host,path,porta)
 
-
-    # exit()
-    # if ":" in url:
-    #   a1=url.index(':')
-    #   if url[a1+1]!='/':
-    #     print('Tem : pontos')
-    #     a=url.index(':',5)
-    #     a=a+1
-    #     porta=url[a:len(url)]
-    #   # elif:
-    #   else:
-    #     print(url)
-    #     porta='80'
-      
-    # else:
-    #     porta='80'
-    # descarta,host=url.split('//')
-    # auxiliarcolculo=url.split('/')
-
-    # if len(auxiliarcolculo)>3:
-    #   resultado=host.index('/')
-    #   caminhoincompleto=host[resultado:len(host)]
-      
-    # else:
-     
-    #   caminhoincompleto=''
-    # enderco=host.split('/')
-    
-    # host=enderco[0]
-  
-    # path=''.join(caminhoincompleto)
-    
-  
-    # if(host[0]!='1'):
-    #   pattern="www."
-    #   if pattern not in host:
-    #     host='www.'+host
-        
-    # else:
-    #   print("Antes de", path)
-    #   exit()
-    #   ende.servidorconect(host,path,porta)
-      
-    # ende.requisicaohost(host,path,porta)
 if __name__ == "__main__":
   while(1):
     main()
